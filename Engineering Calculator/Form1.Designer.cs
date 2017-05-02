770c5e4254b0ca027417811070e8d936df69ebcb
matt12384 <matt12384@live.co.uk>
2017-05-02 20:17:52 +0100

    first commit

---
+++ Engineering Calculator/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace Engineering_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.d1_txt = new System.Windows.Forms.TextBox();
+            this.d2_txt = new System.Windows.Forms.TextBox();
+            this.r_txt = new System.Windows.Forms.TextBox();
+            this.P_txt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.K_txt = new System.Windows.Forms.TextBox();
+            this.n_txt = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // d1_txt
+            // 
+            this.d1_txt.Location = new System.Drawing.Point(48, 19);
+            this.d1_txt.Name = "d1_txt";
+            this.d1_txt.Size = new System.Drawing.Size(100, 20);
+            this.d1_txt.TabIndex = 0;
+            this.d1_txt.TextChanged += new System.EventHandler(this.d1_txt_TextChanged);
+            this.d1_txt.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.d1_txt_KeyPress);
+            // 
+            // d2_txt
+            // 
+            this.d2_txt.Location = new System.Drawing.Point(48, 45);
+            this.d2_txt.Name = "d2_txt";
+            this.d2_txt.Size = new System.Drawing.Size(100, 20);
+            this.d2_txt.TabIndex = 1;
+            this.d2_txt.TextChanged += new System.EventHandler(this.d2_txt_TextChanged);
+            this.d2_txt.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.d2_txt_KeyPress);
+            // 
+            // r_txt
+            // 
+            this.r_txt.Location = new System.Drawing.Point(205, 19);
+            this.r_txt.Name = "r_txt";
+            this.r_txt.Size = new System.Drawing.Size(100, 20);
+            this.r_txt.TabIndex = 4;
+            // 
+            // P_txt
+            // 
+            this.P_txt.Location = new System.Drawing.Point(643, 182);
+            this.P_txt.Name = "P_txt";
+            this.P_txt.Size = new System.Drawing.Size(100, 20);
+            this.P_txt.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(435, 164);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Calculate Drawing Force";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "d1, (m)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "d2, (m)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(163, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "K, (Pa)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(189, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "n";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(189, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(10, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "r";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(603, 185);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "P, (N)";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 19);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(142, 21);
+            this.comboBox1.TabIndex = 13;
+            this.comboBox1.Text = "Select Material";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // K_txt
+            // 
+            this.K_txt.Location = new System.Drawing.Point(208, 20);
+            this.K_txt.Name = "K_txt";
+            this.K_txt.Size = new System.Drawing.Size(100, 20);
+            this.K_txt.TabIndex = 15;
+            this.K_txt.TextChanged += new System.EventHandler(this.K_txt_TextChanged);
+            this.K_txt.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.K_txt_KeyPress);
+            // 
+            // n_txt
+            // 
+            this.n_txt.Location = new System.Drawing.Point(208, 46);
+            this.n_txt.Name = "n_txt";
+            this.n_txt.Size = new System.Drawing.Size(100, 20);
+            this.n_txt.TabIndex = 16;
+            this.n_txt.TextChanged += new System.EventHandler(this.n_txt_TextChanged);
+            this.n_txt.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.n_txt_KeyPress);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Engineering_Calculator.Properties.Resources.Drawing_Force_Diagram;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(417, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.d2_txt);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.d1_txt);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.r_txt);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Location = new System.Drawing.Point(435, 1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(317, 74);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Diameter and Area Reduction";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Controls.Add(this.K_txt);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.n_txt);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(435, 81);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(317, 77);
+            this.groupBox2.TabIndex = 19;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Material Selection";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(435, 193);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 23);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "Clear All";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(762, 231);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.P_txt);
+            this.Name = "Form1";
+            this.Text = "Wire Drawing Force Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox d1_txt;
+        private System.Windows.Forms.TextBox d2_txt;
+        private System.Windows.Forms.TextBox r_txt;
+        private System.Windows.Forms.TextBox P_txt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox K_txt;
+        private System.Windows.Forms.TextBox n_txt;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Engineering Calculator/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Engineering_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public double d1, d2, A1, A2, K, n, r, P;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Aluminium Alloy 6061-T6");
+            comboBox1.Items.Add("Brass Annealed");
+            comboBox1.Items.Add("Low Carbon Steel");
+            comboBox1.Items.Add("Other");
+
+            K_txt.ReadOnly = true;
+            n_txt.ReadOnly = true;
+            r_txt.ReadOnly = true;
+            P_txt.ReadOnly = true;
+
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+        }
+
+        private void d1_txt_TextChanged(object sender, EventArgs e)
+        {
+            if (d1_txt.Text == ".")
+            {
+                d1_txt.Text = "";
+            }
+
+            if (d1_txt.Text != "")
+            {
+                d1 = double.Parse(d1_txt.Text);
+                A1 = Math.Pow(d1, 2) * ((Math.PI) / 4);
+            }
+
+            if ((d1_txt.Text != "") && (d2_txt.Text != ""))
+            {
+                r = ((A1 - A2) / A1);
+                r_txt.Text = r.ToString();
+            }
+            else
+                r_txt.Text = "";
+
+            if ((r > 0.63) || (0 >= r))
+            {
+                r_txt.BackColor = Color.Red;
+            }
+            else
+                r_txt.BackColor = DefaultBackColor;
+
+            if (r_txt.Text == "")
+            {
+                r_txt.BackColor = DefaultBackColor;
+            }
+        }
+
+        private void d1_txt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void d2_txt_TextChanged(object sender, EventArgs e)
+        {
+            if (d2_txt.Text == ".")
+            {
+                d2_txt.Text = "";
+            }
+
+            if (d2_txt.Text != "")
+            {
+                d2 = double.Parse(d2_txt.Text);
+                A2 = Math.Pow(d2, 2) * ((Math.PI) / 4);
+            }
+
+            if ((d1_txt.Text != "") && (d2_txt.Text != ""))
+            {
+                r = ((A1 - A2) / A1);
+                r_txt.Text = r.ToString();
+            }
+            else
+                r_txt.Text = "";
+
+            if ((r > 0.63) || (0 >= r))
+            {
+                r_txt.BackColor = Color.Red;
+            }
+            else
+                r_txt.BackColor = DefaultBackColor;
+    
+            if (r_txt.Text == "")
+            {
+               r_txt.BackColor = DefaultBackColor;
+            }
+        }
+
+        private void d2_txt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            K_txt.BackColor = DefaultBackColor;
+            n_txt.BackColor = DefaultBackColor;
+
+            if (comboBox1.SelectedItem.ToString() == "Aluminium Alloy 6061-T6")
+            {
+                K = 410000000;
+                n = 0.05;
+
+                K_txt.Text = K.ToString();
+                n_txt.Text = n.ToString();
+
+                K_txt.ReadOnly = true;
+                n_txt.ReadOnly = true;
+            }
+
+            if (comboBox1.SelectedItem.ToString() == "Brass Annealed")
+            {
+                K = 900000000;
+                n = 0.49;
+
+                K_txt.Text = K.ToString();
+                n_txt.Text = n.ToString();
+
+                K_txt.ReadOnly = true;
+                n_txt.ReadOnly = true;
+            }
+
+            if (comboBox1.SelectedItem.ToString() == "Low Carbon Steel")
+            {
+                K = 530000000;
+                n = 0.26;
+
+                K_txt.Text = K.ToString();
+                n_txt.Text = n.ToString();
+
+                K_txt.ReadOnly = true;
+                n_txt.ReadOnly = true;
+            }
+
+            if (comboBox1.SelectedItem.ToString() == "Other")
+            {
+                K_txt.Text = "";
+                n_txt.Text = "";
+
+                K_txt.ReadOnly = false;
+                n_txt.ReadOnly = false;
+
+                K_txt.BackColor = Color.LightGreen;
+                n_txt.BackColor = Color.LightGreen;
+
+                MessageBox.Show("Please enter your desired values in the highlighted boxes.");                    
+            }
+        }
+
+        private void K_txt_TextChanged(object sender, EventArgs e)
+        {
+            if (K_txt.Text == ".")
+            {
+                K_txt.Text = "";
+            }
+
+            if (K_txt.Text != "")
+            {
+                K = double.Parse(K_txt.Text);
+            }
+        }
+
+        private void K_txt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void n_txt_TextChanged(object sender, EventArgs e)
+        {
+            if (n_txt.Text == ".")
+            {
+                n_txt.Text = "";
+            }
+
+            if (n_txt.Text != "")
+            {
+                n = double.Parse(n_txt.Text);
+            }
+        }
+
+        private void n_txt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            P = (A2 * ((K) / (n + 1)) * (Math.Pow((Math.Log(A1 / A2)), (n + 1))));
+            P_txt.Text = P.ToString();
+
+            if (r > 0.63)
+            {
+                MessageBox.Show("r value is greater than 0.63, please adjust area values.");
+            }
+
+            if (0 > r)
+            {
+                MessageBox.Show("r value is less than 0, please adjust area values.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            d1_txt.Text = "";
+            d2_txt.Text = "";
+            r_txt.Text = "";
+            K_txt.Text = "";
+            n_txt.Text = "";
+            P_txt.Text = "";
+            comboBox1.Text = "Select Material";
+        }
+    }
+}
